afd6ddf1f08aaf78546c1db5c460df8416117363
Bjarki Viðar Kristjánsson <bvk1@hi.is>
2018-01-30 16:42:05 +0000

    lint errors fixed and more

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
+
 const art = require('./articles.js');
+
 const app = express();
 const path = require('path');
 
@@ -10,10 +12,8 @@ const port = 3000;
 
 app.set('view engine', 'ejs');
 
-
-
 app.listen(port, hostname, () => {
-  //console.log(`Server running at http://${hostname}:${port}/`);
+  // console.log(`Server running at http://${hostname}:${port}/`);
 });
 
 app.use('/img', express.static(path.join(__dirname, 'articles/img')));
@@ -22,10 +22,10 @@ app.use(express.static('public'));
 app.use('/', art);
 
 
-app.use((req,res) =>{
-  res.status(404).render('error', { title: 'Fannst ekki', messege: 'Ó nei, efnið finnst ekki!'});
+app.use((req, res) => {
+  res.status(404).render('error', { title: 'Fannst ekki', messege: 'Ó nei, efnið finnst ekki!' });
 });
 
-app.use((req,res) =>{
-  res.status(500).render('error', { title: 'Villa kom upp', messege: ''});
+app.use((req, res) => {
+  res.status(500).render('error', { title: 'Villa kom upp', messege: '' });
 });
